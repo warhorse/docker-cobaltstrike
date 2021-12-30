59fb7dad505df8a0af7cd26c7380d46cd6ee726e
Ralph <ralph@thedarkcloud.net>
2021-12-30 12:49:01 -0500

    Update docker-entrypoint.sh
    Fixed URLS

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@
 IPADDRESS="$(ip address | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d''/)"
 
 cd /opt/cobaltstrike
-token=`curl -s https://www.cobaltstrike.com/download -d "dlkey=${COBALTSTRIKE_KEY}" | grep 'href="/downloads/' | cut -d '/' -f3`
+token=`curl -s https://download.cobaltstrike.com/download -d "dlkey=${COBALTSTRIKE_KEY}" | grep 'href="/downloads/' | cut -d '/' -f3`
 curl -s https://download.cobaltstrike.com/downloads/${token}/cobaltstrike-dist.tgz -o /tmp/cobaltstrike.tgz
 tar zxf /tmp/cobaltstrike.tgz -C /opt
 echo $COBALTSTRIKE_KEY > ~/.cobaltstrike.license
