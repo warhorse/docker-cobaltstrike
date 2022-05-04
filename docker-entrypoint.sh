282bfdbb924f65e51a4f7a33cc7ce89ad79867c6
Ralph <ralph@thedarkcloud.net>
2022-05-03 22:37:54 -0700

    Update docker-entrypoint.sh
    fix for new version

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,8 +7,7 @@ IPADDRESS="$(ip address | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cu
 
 cd /opt/cobaltstrike
 token=`curl -s https://download.cobaltstrike.com/download -d "dlkey=${COBALTSTRIKE_KEY}" | grep 'href="/downloads/' | cut -d '/' -f3`
-curl -s https://download.cobaltstrike.com/downloads/${token}/cobaltstrike-dist.tgz -o /tmp/cobaltstrike.tgz
+curl -s https://download.cobaltstrike.com/downloads/${token}/latest46/cobaltstrike-dist.tgz -o /tmp/cobaltstrike.tgz
 tar zxf /tmp/cobaltstrike.tgz -C /opt
-echo $COBALTSTRIKE_KEY > ~/.cobaltstrike.license
-/opt/cobaltstrike/update
+echo ${COBALTSTRIKE_KEY} | opt/cobaltstrike/update
 /opt/cobaltstrike/teamserver $IPADDRESS ${COBALTSTRIKE_PASS} /opt/cobaltstrike/profiles/${COBALTSTRIKE_PROFILE}.profile ${COBALTSTRIKE_EXP}
