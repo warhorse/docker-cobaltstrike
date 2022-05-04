fc3f33d1ec4324da9c12a76082006d6482d96cb7
Ralph <ralph@thedarkcloud.net>
2022-05-03 22:43:34 -0700

    Update docker-entrypoint.sh
    fix /

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,5 +9,5 @@ cd /opt/cobaltstrike
 token=`curl -s https://download.cobaltstrike.com/download -d "dlkey=${COBALTSTRIKE_KEY}" | grep 'href="/downloads/' | cut -d '/' -f3`
 curl -s https://download.cobaltstrike.com/downloads/${token}/latest46/cobaltstrike-dist.tgz -o /tmp/cobaltstrike.tgz
 tar zxf /tmp/cobaltstrike.tgz -C /opt
-echo ${COBALTSTRIKE_KEY} | opt/cobaltstrike/update
+echo ${COBALTSTRIKE_KEY} | /opt/cobaltstrike/update
 /opt/cobaltstrike/teamserver $IPADDRESS ${COBALTSTRIKE_PASS} /opt/cobaltstrike/profiles/${COBALTSTRIKE_PROFILE}.profile ${COBALTSTRIKE_EXP}
